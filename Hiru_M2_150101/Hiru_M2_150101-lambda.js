788048d738e5c19b5fac41b1975004e394908320
hirudinee <35219641+hirudinee@users.noreply.github.com>
2018-01-15 14:26:52 +0530

    sns added

--- Hiru_M2_150101/Hiru_M2_150101-lambda.js
+++ Hiru_M2_150101/Hiru_M2_150101-lambda.js
@@ -1,12 +1,41 @@
 let AWS = require('aws-sdk');
-exports.handler = function(event, context, callback){
+const sns = new AWS.SNS();
+exports.handler = function (event, context, callback) {
 
 
 
-	
-	callback(null,'Successfully executed');
+
+	callback(null, 'Successfully executed');
 
 	console.log('Successfully executed');
+
+	sns.publish({
+		Message: 'This is a test message',
+		Subject: 'this is a test',
+		MessageAttributes: {
+			'key01': {
+				DataType: 'String',
+				StringValue: 'val01'
+			},
+			'key02': {
+				DataType: 'Number',
+				StringValue: '001'
+			},
+			'key03': {
+				DataType: 'Binary',
+				StringValue: 'false'
+			},
+		},
+		MessageStructure: 'String',
+		TopicArn: 'arn:aws:sns:us-east-1:480964559519:hiru_topic'
+	}).promise()
+		.then(data => {
+			// your code goes here
+		})
+		.catch(err => {
+			// error handling goes here
+		});
+
 
 
 }
