09662bec17a6f378c3e5d12dde7cae465e5378cb
hirudinee <35219641+hirudinee@users.noreply.github.com>
2018-01-15 13:53:45 +0530

    repo name chane

---
+++ Hiru_M2_150101/Hiru_M2_150101-lambda.js
@@ -0,0 +1,12 @@
+let AWS = require('aws-sdk');
+exports.handler = function(event, context, callback){
+
+
+
+	
+	callback(null,'Successfully executed');
+
+	console.log('Successfully executed');
+
+
+}
